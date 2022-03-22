f2d46daff01c75a893bcf54dcf2e11af2290fed9
Nakaw <nakawyoung@gmail.com>
2022-03-22 10:31:09 -0400

    first commit

---
+++ src/main/java/company/MyWebapp/MyWebappApplication.java
@@ -0,0 +1,13 @@
+package company.MyWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/company/MyWebapp/MyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package company.MyWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
