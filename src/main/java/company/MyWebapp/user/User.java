4f94b70961e67d159378d9dea85e2b62158979e3
Nakaw <nakawyoung@gmail.com>
2022-03-24 10:39:16 -0400

    Fixed typo in email for User entity and remade the table for Users in MySql, also made a test for the repository

--- src/main/java/company/MyWebapp/user/User.java
+++ src/main/java/company/MyWebapp/user/User.java
@@ -8,7 +8,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(nullable = false, unique = true, length=15)
+    @Column(nullable = false, unique = true, length=45)
     private String email;
     @Column(length=15, nullable = false)
     private String password;
--- src/test/java/company/MyWebapp/UserRepositoryTests.java
+++ src/test/java/company/MyWebapp/UserRepositoryTests.java
@@ -1,4 +1,32 @@
 package company.MyWebapp;
 
+import company.MyWebapp.user.User;
+import company.MyWebapp.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
 public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user=new User();
+        user.setEmail("tester@gmail.com");
+        user.setPassword("brotherbear123");
+        user.setFirstName("test");
+        user.setLastName("er");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
 }
